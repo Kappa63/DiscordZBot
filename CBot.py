7a084735c32e295f2889e117f620b970bd4bbc4f
Karim Q <kareem.qiblawi2@outlook.com>
2020-11-16 00:20:39 +0200

    sw

--- CBot.py
+++ CBot.py
@@ -17,6 +17,8 @@ import giphy_client
 import twitter
 import mal
 import malclient
+import COVID19Py
+import datetime
 
 Mdb = "mongodb+srv://Kappa:85699658@cbotdb.exsit.mongodb.net/CBot?retryWrites=true&w=majority"
 Cls = MongoClient(Mdb)
@@ -36,6 +38,8 @@ Reddit = praw.Reddit(client_id = "ntnBVsoqGHtoNw", client_secret = "ZklNqu4BQK4j
 MClient = malclient.Client()
 MClient.init(access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImY5OWNmNDExZjY5NDMwMGY0ZjcwMDAyM2JlOTYyYTkyYjQxYzUwNGQ0NzA0MzQyOWYxMjEyZmNkOTBmNWNiZjlkNmFmZWNkYWRmMTViMzdjIn0.eyJhdWQiOiIyYjcwMWQzNjY5NzFmYTFmMTgyYmZkNTBkMTUxNzJhZSIsImp0aSI6ImY5OWNmNDExZjY5NDMwMGY0ZjcwMDAyM2JlOTYyYTkyYjQxYzUwNGQ0NzA0MzQyOWYxMjEyZmNkOTBmNWNiZjlkNmFmZWNkYWRmMTViMzdjIiwiaWF0IjoxNjA1MTc4ODM5LCJuYmYiOjE2MDUxNzg4MzksImV4cCI6MTYwNzc3MDgzOSwic3ViIjoiNzQ5Njc5MSIsInNjb3BlcyI6W119.B8X86ggNC43bZwzKF6993WSnY1AUGQ2wgdxbL2kRhGPJAm4M3epzbTixTxxs3RmWOsUfypoU3U2vnlYs69enzwsdGxzpoLh-hO_Mav4kSTxeXqrvPk23_7fSyC1Q8AOFE_EszhI6DG67BcFAZWVdgFia8th6vZ_7HTugWd9dDrf1PIBDfNrpWrsTs1tUImTbsZ41Y_19uT2p3-oTpmQY_YwSbLxgzkdVZmASWdDkXyFjTNnYW5y_fCDYQrDJrNrId5Dvm1N02d66TNaJgyDn86L0Dr-lYqjU9qM45agHff4T8MpkIzqzA3pKT874QUOW5QXks46-9JaCCpSB-nIrfQ")
 MClient.refresh_bearer_token(client_id = "2b701d366971fa1f182bfd50d15172ae", client_secret = "e01505a84d5e611e2e59b66f0dc245888656104b1529e1a25954d8ff51780f5c", refresh_token = "def50200b66cd79fef2e2b550556891e5d1a4c7774d4db62ff64a49900570a29f94680d1a93ba950d8af2f3b98a4b8af587e2fd939cb94f5a8ce5fc4498a26469da1973224c916e11ed3fbb73d7cfca981c865c3cd9d611674d113159746a6759cfcf4a646132332007b3228f7c83a761ef1226693a7b9e27c6d6b621602943c690ce1351f993088872976c25fa680f1622e7bbf38000fdc00a0e7557f4ef70e3cc4af93ea213ef090c155a9deb37a7c3db56fcabaef4a13783bb5d2a22cf100e5928292df6cb468b63497ad74b4a93fe3d2d086043bf51c9a58fd5341f519fa3a6946cd8ada2c554fffce8d59e35380ddbfb341d7777056e4c0da0a87a1e2cd5d0944ccc54f6593f2ccb5345cb827e0587cb07e66ae931d0e74d14f1a295110f5a4b402ab9a53b244168d629bc21925fb4aefc9aa201d48ccdff77d36557fb49bd5e89ce979aeb0c22972f6cdc5bc1dc2dcceb38b137a305b647bc1ccd3c18eac108cb5159e1c64ef17dd4059d64dd1b53c2000a74f8b4013a90e9325be2cc30ded29d8b72907c7")
+
+Cov = COVID19Py.COVID19(data_source = "jhu")
 
 GClient = "ZH1xoGH0XUffrtqFKdj3kD4YrVoZvb8i"
 GApi = giphy_client.DefaultApi()
@@ -121,12 +125,15 @@ async def SendH(ctx, *args):
         HEm.add_field(name = "zlist: ", value = "Returns all added words/phrases", inline = False)
         HEm.add_field(name = "zstats (@) (Word): ", value = "Returns stats for word(s)/phrase(s)", inline = False)
         HEm.add_field(name = "ztotal (Word): ", value = "Returns the total number of times word(s)/phrase(s) have been said on server", inline = False)
+        HEm.add_field(name = "zreset: ", value = "Reset everything (this is irreversable)", inline = False)
         HEm.set_footer(text = "Note: Counting is limited to 10 per Message to reduce spam incentives")
         await ctx.message.channel.send(embed = HEm)
     elif "".join(args).lower() == "misc" or "".join(args).lower() == "misc." or "".join(args).lower() == "miscellaneous":
         HEm = discord.Embed(title = "**ZBot Misc. Help**", description = "\u200b", color = 0x0af531)
         HEm.add_field(name = "zfry (Image Attachment/Image Url): ", value = "Deep fries the image", inline = False)
         HEm.add_field(name = "zfry profile (@): ", value = "Deep fries the avatar", inline = False)
+        HEm.add_field(name = "zcovid: ", value = "Returns the worldwide status of Covid-19", inline = False)
+        HEm.add_field(name = "zcovid (Country): ", value = "Returns the status of Covid-19 in country", inline = False)
         HEm.add_field(name = "zreddit (Subreddit Name): ", value = "Returns a RANDOM post from the top 100 posts in hot from any subreddit", inline = False)
         HEm.add_field(name = "ztwitter (User @): ", value = "Returns the user profile", inline = False)
         HEm.add_field(name = "ztwitter search (Username): ", value = "Searches for 10 users related to search argument", inline = False)
@@ -728,6 +735,33 @@ async def nHen(ctx, *args):
     else:
         await ctx.message.channel.send("No arguments :no_mouth:")
 
+@DClient.command(name = "covid")
+@commands.check(ChBot)
+@commands.cooldown(1, 3, commands.BucketType.guild)
+async def CovSt(ctx, *args):
+    if args:
+        CovDLoc = Cov.getLocations()
+        for TCov in CovDLoc:
+            if TCov["country"].lower() == " ".join(args).lower():
+                FounCon = True
+                CEm = discord.Embed(title = f'{" ".join(args)} Covid-19 Status', description = f'As of {datetime.date.today()}', color = 0xbd9400)
+                CEm.add_field(name = "Population: ", value = f'{TCov["country_population"]:,}', inline = False)
+                CEm.add_field(name = "Confirmed: ", value = f'{TCov["latest"]["confirmed"]:,}', inline = False)
+                CEm.add_field(name = "Deaths: ", value = f'{TCov["latest"]["deaths"]:,}', inline = False)
+                CEm.add_field(name = "Recovered: ", value = f'{TCov["latest"]["recovered"]:,}', inline = False)
+                CEm.set_footer(text = "Note: Data may not be completely accurate\n\nData From John Hopkins University (JHU)")
+                break
+        if not FounCon:
+            await ctx.message.channel.send("Country not found :pensive:")
+    else: 
+        CovDWW = Cov.getLatest()
+        CEm = discord.Embed(title = "Worldwide Covid-19 Status", description = f'As of {datetime.date.today()}', color = 0xbd9400)
+        CEm.add_field(name = "Confirmed: ", value = f'{CovDWW["confirmed"]:,}', inline = False)
+        CEm.add_field(name = "Deaths: ", value = f'{CovDWW["deaths"]:,}', inline = False)
+        CEm.add_field(name = "Recovered: ", value = f'{CovDWW["recovered"]:,}', inline = False)
+        CEm.set_footer(text = "Note: Data may not be completely accurate\n\nData From John Hopkins University (JHU)")
+    await ctx.message.channel.send(embed = CEm)
+
 @DClient.command(name = "reddit")
 @commands.check(ChBot)
 @commands.cooldown(1, 3, commands.BucketType.user)
@@ -890,10 +924,12 @@ async def LWord(ctx):
 @commands.check(ChSer)
 @commands.cooldown(1, 1, commands.BucketType.user)
 async def ReAll(ctx):
-    DbB = Col.find({"IDg":str(ctx.guild.id)})
-    for DbG in DbB:
-        Col.delete_one(DbG)
-        
+    if Col.count_documents({"IDd":"GuildInfo","IDg":str(ctx.guild.id),"Setup":"Done"}) > 0:
+        DbB = Col.find({"IDg":str(ctx.guild.id)})
+        for DbG in DbB:
+            Col.delete_one(DbG)
+    await ctx.message.channel.send("All info was cleared successfully :thumbsup:")
+
 @DClient.command(name = "total")
 @commands.check(ChBot)
 @commands.check(ChSer)
@@ -1102,9 +1138,10 @@ async def on_member_remove(member):
 
 @DClient.event
 async def on_guild_remove(guild):
-    DbB = Col.find({"IDg":str(guild.id)})
-    for DbG in DbB:
-        Col.delete_one(DbG)
+    if Col.count_documents({"IDd":"GuildInfo","IDg":str(guild.id),"Setup":"Done"}) > 0:
+        DbB = Col.find({"IDg":str(guild.id)})
+        for DbG in DbB:
+            Col.delete_one(DbG)
 
 @DClient.event
 async def on_command_error(ctx, error):
