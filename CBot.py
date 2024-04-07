8648aca8e9301eb238c45034a4095e7df1e9231c
Karim Q <kareem.qiblawi@gmail.com>
2024-04-07 05:07:11 +0300

    test

--- CBot.py
+++ CBot.py
@@ -1,28 +1,42 @@
 import discord
 from discord.ext import commands
-from Setup import Ignore, IsBot
-import dbl
+# from Setup import Ignore, IsBot
+# import dbl
 from dotenv import load_dotenv
 import os
-
+from Setup import Cogs
 load_dotenv()
 
-REqInt = discord.Intents.default()
-REqInt.members = True
 
-DClient = commands.Bot(case_insensitive=True, command_prefix=["z", "Z"], help_command=None, intents=REqInt)
+class DClient(commands.Bot):
+    def __init__(self, Cogs):
+        REqInt = discord.Intents.all()
+        REqInt.members = True
+        self.Cogs = Cogs
+        super().__init__(case_insensitive=True, command_prefix=["Z","z"], intents=REqInt)
+   
+    async def setup_hook(self) -> None:
+        for Cog in self.Cogs: 
+            await self.load_extension(Cog)
+        print("Cogs Loaded...")
+        # print(self.guilds)
+        return await super().setup_hook()
+    
 
-TClient = dbl.client.DBLClient(bot=DClient, token=os.getenv("DBL_TOKEN"), autopost=True)
+    # DClient = commands.Bot()
 
-@DClient.check
-async def ChBot(ctx):
-    if ctx.author.bot: raise IsBot("Bot")
-    return True
 
-@DClient.check
-async def ChDM(ctx):
-    if ctx.guild: return True
-    raise Ignore("Ignore")
+# TClient = dbl.client.DBLClient(bot=DClient, token=os.getenv("DBL_TOKEN"), autopost=True)
+
+# @DClient.check
+# async def ChBot(ctx):
+#     if ctx.author.bot: raise IsBot("Bot")
+#     return True
+
+# @DClient.check
+# async def ChDM(ctx):
+#     if ctx.guild: return True
+#     raise Ignore("Ignore")
 
 #_ @DClient.check
 #_ async def ChModDown(ctx):
@@ -31,13 +45,11 @@ async def ChDM(ctx):
 #_     OpenState.close()
 #_     if ("".join(State) == "Down") and ctx.author.id not in [507212584634548254, 443986051371892746, 224809178793771009]: raise Ignore("Ignore")
 #_     return True
+# Cogs = ["Cogs.Misc", "Cogs.MongoDB", "Cogs.Covid", "Cogs.Nasa", "Cogs.Socials", "Cogs.AnimeManga",
+#         "Cogs.HelpInfo", "Cogs.Randomizers", "Cogs.OnlyMods", "Cogs.MainEvents", "Cogs.Rule34",
+#         "Cogs.Images","Cogs.WrittenStuff", "Cogs.Movies", "Cogs.Games", "Cogs.GameAPIs", "Cogs.Google"]
+    
 
-Cogs = ["Cogs.Misc", "Cogs.MongoDB", "Cogs.Covid", "Cogs.Nasa", "Cogs.Socials", "Cogs.AnimeManga",
-        "Cogs.HelpInfo", "Cogs.Randomizers", "Cogs.OnlyMods", "Cogs.MainEvents", "Cogs.Rule34",
-        "Cogs.Images","Cogs.WrittenStuff", "Cogs.Movies", "Cogs.Games", "Cogs.GameAPIs", "Cogs.Google"]
+BotClient = DClient(Cogs=Cogs)
 
-if __name__ != "__main__":
-    for Cog in Cogs: DClient.load_extension(Cog)
-    print("Cogs Loaded...")
-
-DClient.run(os.getenv("DISCORD_SECRET"))
+BotClient.run(os.getenv("DISCORD_TOKEN_TIA"))
